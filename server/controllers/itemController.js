5f4199fc370c8af4848a4f0e361d030d6190f4fe
eric-baldwin <erictbaldwinjr@gmail.com>
2023-09-11 18:35:56 -0600

    seeded and added routes and controllers

---
+++ server/controllers/itemController.js
@@ -0,0 +1,60 @@
+const knex = require('../database/knexConnection');
+
+module.exports = {
+
+    createItem: async (req, res) => {
+        try {
+            const newItem = {
+                userId: req.body.userId,
+                itemName: req.body.itemName,
+                description: req.body.description,
+                quantity: req.body.quantity
+            };
+
+            await knex('items').insert(newItem);
+            res.status(201).json({ message: 'Item created successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    getAllItems: async (req, res) => {
+        try {
+            const items = await knex('items').select();
+            res.status(200).json(items);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    getItemById: async (req, res) => {
+        try {
+            const item = await knex('items').where('id', req.params.id).first();
+            if (item) {
+                res.status(200).json(item);
+            } else {
+                res.status(404).json({ message: 'Item not found' });
+            }
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    updateItem: async (req, res) => {
+        try {
+            await knex('items').where('id', req.params.id).update(req.body);
+            res.status(200).json({ message: 'Item updated successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    deleteItem: async (req, res) => {
+        try {
+            await knex('items').where('id', req.params.id).delete();
+            res.status(200).json({ message: 'Item deleted successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+};
---
+++ server/controllers/userController.js
@@ -0,0 +1,60 @@
+const knex = require('../database/knexConnection');
+
+module.exports = {
+
+    createUser: async (req, res) => {
+        try {
+            const newUser = {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                username: req.body.username,
+                password: req.body.password
+            };
+
+            await knex('users').insert(newUser);
+            res.status(201).json({ message: 'User created successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    getAllUsers: async (req, res) => {
+        try {
+            const users = await knex('users').select();
+            res.status(200).json(users);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    getUserById: async (req, res) => {
+        try {
+            const user = await knex('users').where('id', req.params.id).first();
+            if (user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({ message: 'User not found' });
+            }
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    updateUser: async (req, res) => {
+        try {
+            await knex('users').where('id', req.params.id).update(req.body);
+            res.status(200).json({ message: 'User updated successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    deleteUser: async (req, res) => {
+        try {
+            await knex('users').where('id', req.params.id).delete();
+            res.status(200).json({ message: 'User deleted successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+};
